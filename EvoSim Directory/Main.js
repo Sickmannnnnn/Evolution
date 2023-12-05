689c0a9a55406c38df58566b8b811afb8d929b74
Sickmannnnnn <ZACK.SICKMANN@ICLOUD.COM>
2023-12-04 20:07:28 -0500

    Co-authored-by: Krosauer <Krosauer@users.noreply.github.com>

--- EvoSim Directory/Main.js
+++ EvoSim Directory/Main.js
@@ -7,8 +7,11 @@ let yCanvas = 150;
 //with #myCanvas height = 526px
 let startingPlants = 10;
 let drawInterval;
+let predateInterval;
 let dieInterval;
 let reproduceInterval;
+let evolutionRate = .01;
+let count = 0;
 
 let canvas;
 let context;
@@ -31,49 +34,37 @@ function setup() {
     for(let i = 0; i < startingPlants; i++){
         let org = new Organism(Math.random()*xCanvas, Math.random()*yCanvas, 0, Math.random()*20 + 20, Math.random()*2 + 1)
         organisms.push(org);
-        console.log(org.level);
     }
-    console.log(organisms[0].level)
     clearIntervals();
-    console.log(organisms[0].level)
     setIntervals();
-    for(let i = 0; i < organisms.length; i++){
-        console.log(organisms[i].level);
-    }
 }
 
 function setIntervals(){
-    drawInterval = setInterval(draw,1000);
+    predateInterval = setInterval(predate, 499);
+    drawInterval = setInterval(draw,5000);
 }
 function clearIntervals(){
     clearInterval(drawInterval);
 }
 
 function draw() {
-    for(let org in organisms){
-        console.log(org.level);
-    }
+    die();
+    predate();
+    count++;
+    console.log(count + "-----------------------------")
     context.clearRect(0,0, canvas.width, canvas.height);
     for(let i = 0; i < organisms.length; i++){
         let org = organisms[i];
         context.beginPath();
-        //console.log("path");
         context.ellipse(org.x,org.y,orgDiameter,orgDiameter,0,0,2 * Math.PI);
-        let isLev = org.level === 0;
-        console.log(isLev);
-        console.log(org.level);
-        if(isLev){
+        if(org.level === 0){
             context.fillStyle = "green";
-            console.log("color");
         }
         if(org.level === 1){
             context.fillStyle = "yellow";
         }
         context.fill();
-        //console.log("fill");
     }
-    die();
-    predate();
     reproduce();
 }
 
@@ -90,7 +81,7 @@ function reproduce(){
         if(horny.includes(org1)){
             for(let j = i+1; j < organisms.length; j++){
                 let org2 = organisms[j];
-                if(horny.includes(org2) && inRange(org1,org2,reproduceRange)){
+                if(org1.level === org2.level && horny.includes(org2) && inRange(org1,org2,reproduceRange)){
                     let b = average(org1.birthDistance, org2.birthDistance);
                     let xPos = average(org1.x,org2.x) + Math.random()*b - b/2;
                     let yPos = average(org1.y, org2.y) + Math.random()*b - b/2;
@@ -107,10 +98,10 @@ function reproduce(){
                     //console.log('average birth distance: ' + birthDistanceSum / organisms.length);
                     //console.log('maximum children: ' + maxChildSum / organisms.length);
                     let level;
-                    if(rand > .8){
+                    if(rand > 1 - evolutionRate){
                         level = org1.level + 1;
                     }
-                    else if(rand < 0.2 && org1.level !== 0){
+                    else if(rand < evolutionRate && org1.level !== 0){
                         level = org1.level - 1;
                     }
                     else{
@@ -151,6 +142,7 @@ function predate(){
                 if(!temp.includes(org2) && org1.level - 1 === org2.level && inRange(org1,org2,org1.diameter + org2.diameter)){
                     temp.push(org2);
                     org1.food++;
+                    console.log("Predation Death");
                 }
             }
         }
@@ -159,7 +151,6 @@ function predate(){
         for(let j = 0; j < temp.length; j++){
             if(organisms[i] === temp[j]){
                 organisms.splice(i,1);
-                console.log("predate is probably working");
                 break;
             }
         }
@@ -171,21 +162,27 @@ function die(){
     for(let i = 0; i < organisms.length; i++){
         let org1 = organisms[i];
         org1.timeAlive++;
+        for(let j = i+1; j < organisms.length; j++) {
+            let org2 = organisms[j];
+            if (count > 8) {
+                console.log("Shit" + org1.level === org2.level && inRange(org1, org2, average(org1.diameter, org2.diameter)))
+            }
+            if (org1.level === org2.level && inRange(org1, org2, average(org1.diameter, org2.diameter))) {
+                temp.push(org1);
+                temp.push(org2);
+                console.log("Population Death");
+                break;
+            }
+        }
         if(org1.timeAlive >= org1.lifeSpan){
             temp.push(org1);
+            console.log("Natural Death");
             break;
         }
         if(org1.level > 0 && org1.food <= 0){
             temp.push(org1);
+            console.log("Food Limited Death");
             break;
-        }
-        for(let j = i+1; j < organisms.length; j++){
-            let org2 = organisms[j]
-            if(org1.level === org2.level && inRange(org1,org2,average(org1.diameter,org2.diameter))){
-                temp.push(org1);
-                temp.push(org2);
-                break;
-            }
         }
     }
     //print(temp)
@@ -193,7 +190,6 @@ function die(){
         for(let j = 0; j < temp.length; j++){
             if(organisms[i] === temp[j]){
                 organisms.splice(i,1);
-                console.log("die is probably working");
                 break;
             }
         }
